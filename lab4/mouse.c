3e7b47d63c5bfd47fc5c97ba05ac166ccf8049e2
Andre Malheiro <up201706280@feup-b105d69.fe.up.pt>
2020-11-06 18:56:02 +0000

    Base code for lab4

---
+++ lab4/mouse.c
@@ -0,0 +1,13 @@
+#include "mouse.h"
+
+int mouse_enable_data_reporting(void){
+
+}
+
+void mouse_ih(void);{
+
+}
+
+int (mouse_subscribe_interrupts)(){
+    
+}
--- lab4/mouse.h
+++ lab4/mouse.h
@@ -1,8 +1,7 @@
-
- #pragman once
- 
- int mouse_enable_data_reporting(void);
- 
- 
- void mouse_ih(void);
+#include <lcom/lcf.h>
+#pragma once
+#include "macros.h"
+int mouse_enable_data_reporting(void);
+void mouse_ih(void);
+int (mouse_subscribe_interrupts)();
  
