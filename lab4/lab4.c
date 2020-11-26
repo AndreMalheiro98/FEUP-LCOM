61b18d18d66bd696374b6361f388575fc01dbbce
Juan Bellon <up201908142@fe.up.pt>
2020-11-26 20:28:26 +0000

    async function done

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 // Any header files included below this line should have been created by you
 
@@ -31,6 +32,8 @@ int main(int argc, char *argv[]) {
 }
 extern uint8_t data;
 extern bool flag;
+extern uint32_t global_int_counter;
+
 #include "mouse.h"
 #include "kbc.h"
 int (mouse_test_packet)(uint32_t cnt) {
@@ -132,9 +135,112 @@ int (mouse_test_packet)(uint32_t cnt) {
 }
 
 int (mouse_test_async)(uint8_t idle_time) {
-    /* To be completed */
-    printf("%s(%u): under construction\n", __func__, idle_time);
-    return 1;
+  if(mouse_enable_data_report()!=0)
+  {
+    printf("Error enabling data report\n");
+    return -1;
+  }
+  else
+    printf("Data report enabled successfully\n");
+  //Subscribing mouse interrupts
+  if(mouse_subscribe_interrupts()==-1)
+  {
+    printf("Error subscribing mouse interrupts\n");
+    return -1;
+  }
+  uint8_t irq_set_timer
+  if (timer_subscribe_int(&irq_set_timer)==1)
+  {
+    printf("Error subscribing timer 0 interrupts\n");
+    return -1;
+  }
+
+  int ipc_status,r;
+  message msg;
+  uint32_t mask=BIT(MOUSE_IRQ), mask2 = BIT(irq_set_timer);
+
+  while (global_int_counter < idle_time * sys_hz()){
+    if( (r=driver_receive(ANY,&msg,&ipc_status)) !=0 )
+    {
+      printf("Driver receive failed with %d\n",r);
+      continue;
+    }
+    if(is_ipc_notify(ipc_status)){
+      switch(_ENDPOINT_P(msg.m_source))
+      {
+        case HARDWARE:
+          if(msg.m_notify.interrupts & mask)
+          {
+            mouse_ih();
+            if(flag)
+             {
+               if(data & BIT(3) && bytes==0)
+                 dados[0]=data;
+               else
+                dados[bytes]=data;
+                bytes++;
+             }
+            else
+               continue;
+             
+            if(bytes==3)
+            {
+              struct packet pacote_dados;
+              createMousePacket(dados,&pacote_dados);
+              mouse_print_packet(&pacote_dados);
+              cnt--;
+              bytes=0;
+            }
+            global_int_counter = 0;
+          }
+          else if (msg.m_notify.interrupts & mask2) 
+            timer_int_handler();
+        break;
+
+        default:
+          break;
+      }
+    }
+  }
+
+  if(mouse_unsubscribe_interrupts()==-1)
+  {
+    printf("Error unsubscribing mouse interrupts\n");
+    return -1;
+  }
+
+  if(timer_unsubscribe_int()!=0)
+  {
+    printf("Error unsubscribing timer 0 interrupts\n");
+    return -1;
+  }
+
+  if(write_command(KBC_WRITE_TO_MOUSE)!=0)
+  {
+    printf("Error writing command 0xD4 to KBC\n");
+    return -1;
+  }
+
+  if(write_command_byte(MOUSE_DISABLE_DATA_REPORT)!=0)
+  {
+    printf("Error writing command 0xF5 to mouse\n");
+    return -1;
+  }
+
+  //Reading return byte from mouse due to writing byte to mouse
+  if(read_return_from_mouse()!=0)
+  {
+    printf("Error reading answer from mouse\n");
+    return -1;
+  }
+  else
+  {
+    printf("Data report successfully disabled\n");
+  }
+
+  return 0;
+  
+
 }
 
 int (mouse_test_gesture)(uint8_t x_len, uint8_t tolerance) {
